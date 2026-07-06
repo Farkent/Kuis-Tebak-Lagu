a2cba519ad5fa754e2769c56db8eac4b1d5d3772
Farkent <farrelkent35@gmail.com>
2026-07-06 23:39:59 +0700

    Use PHP CLI instead of Apache

--- backend/index.php
+++ backend/index.php
@@ -3,7 +3,7 @@
 header("Content-Type: application/json");
 
 echo json_encode([
-    "success" => true,
-    "message" => "Backend API Tebak Lagu berjalan.",
-    "php_version" => PHP_VERSION
+    "status" => "OK",
+    "php" => PHP_VERSION,
+    "server" => "Railway"
 ]);
